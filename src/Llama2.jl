6ff4ea6ef46de0d08547a2ffddc2c75296d9df7b
ConstantConstantin <paul.hertel@aol.de>
2025-12-02 21:23:17 +0100

    Ignore .vscode, change Vector{UInt8} to String and add encode function.

--- src/Llama2.jl
+++ src/Llama2.jl
@@ -1,10 +1,9 @@
 module Llama2
 
-include("tokenizer.jl")
-
-export Tokenizer, TokenIndex, compare_tokens
+export Tokenizer, TokenIndex, compare_tokens, str_lookup, encode
 # Write your package code here.
 include("structs.jl")
+include("tokenizer.jl")
 
 include("decode_transformer.jl")
 
