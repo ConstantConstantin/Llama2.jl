e7ed576f9e98abe9e2195cadd765fc7597d3b0eb
ConstantConstantin <paul.hertel@aol.de>
2025-12-14 14:53:38 +0100

    Revert "Merge pull request #11 from ConstantConstantin/cph/encode"
    This reverts commit 4786460bcb0d8e74fd8f71a54cc58ec7ff0e1a52, reversing
    changes made to 2d25be8f7c3d342dfe25a4be04d85a10dad0a917.

--- src/Llama2.jl
+++ src/Llama2.jl
@@ -5,10 +5,5 @@ include("tokenizer.jl")
 export Tokenizer, TokenIndex, compare_tokens
 # Write your package code here.
 include("structs.jl")
-include("tokenizer.jl")
-
-export Tokenizer, TokenIndex, compare_tokens, str_lookup, encode
-# Write your package code here.
-include("structs.jl")
 
 end
