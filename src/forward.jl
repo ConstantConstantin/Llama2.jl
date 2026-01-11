bcf2937936c5486256c5fcbc4693b1653a472f81
Alexej Jordan <ajordan@hotmail.de>
2026-01-11 05:49:13 +0100

    build fundamental structure for forward

--- src/forward.jl
+++ src/forward.jl
@@ -26,9 +26,7 @@ julia> o
 
 # Developer Notes
 Dimensions of o, x, and w not quite sure yet.
-
 """
-
 function rmsnorm!(o::Vector{T}, x::Vector{T}, w::Vector{T}) where {T<:Float32}
 
     (length(w) != length(o) || length(o) != length(x)) && throw(DimensionMismatch("x, o, and w must have the same dimensions"))
@@ -75,9 +73,7 @@ julia> x
  0.24472845
  0.66524094
 ```
-
 """
-
 function softmax!(x::Vector{Float32})
 
     isempty(x) && throw(ArgumentError("x must not be empty"))
@@ -97,39 +93,110 @@ function softmax!(x::Vector{Float32})
     return nothing
 end
 
-function forward!(transformer::Transformer)
+function forward!(transformer::Transformer, token::Int32, pos::Int32)
 
     config = transformer.config
     weights = transformer.weights
     state = transformer.state
 
-    seq_len = config.seq_len
+    x = state.x
     dim = config.dim
-    n_kv_heads = config.n_kv_heads
-    head_size = div(dim, n_heads)
+    kv_dim = div(config.dim * config.n_kv_heads, config.n_heads)
+    kv_mul = div(config.n_heads, config.n_kv_heads)
     hidden_dim = config.hidden_dim
+    head_size = div(dim, n_heads)
+    seq_len = config.seq_len
 
     # assigning input token embedding to x
-    @views content_row = weights.token_embedding_table[token, :]
-    x .= content_row
-
-    """
-    currently not in our Trainsformerweight Struct? 
-
-    freq_cis_real_row = weights.freq_cis_real[:, pos]
-    freq_cis_imag_row = weights.freq_cis_imag[:, pos]
-    """
+    x .= weights.token_embedding_table[token, :]
 
     for l in 1:config.n_layers
-        rmsnorm!(state.xb, state.x, weights.rms_att_weight[:, l])
+        
+        rmsnorm!(state.xb, x, weights.rms_att_weight[:, l])
+
+        loff = l * seq_len * kv_dim
+        state.k = @view state.key_cache[loff, pos * kv_dim, :]
+        state.v = @view state.value_cache[loff, pos * kv_dim, :]
         # matmul to get q, k, v
 
-        for h in 1:config.n_heads # multi-head attention
+        state.q = weights.wq[l * dim * dim, :, :] * state.xb
+        state.k .= weights.wk[l * dim * kv_dim] * state.xb
+        state.v .= weights.wv[l * dim * kv_dim] * state.xb
+
+        for i in 1:2:dim
+
+            head_dim = i % head_size
+            freq = 1.0f / (10000.0f^(head_dim / head_size))
+            val = pos * freq
+            fcr = cos(val)
+            fci = sin(val)
             
+            for v in 1:(1 + (i < kv_dim))
+                vec = v == 0 ? state.q : state.k
+                v0 = vec[i]
+                v1 = vec[i + 1]
+                vec[i] = v0 * fcr - v1 * fci
+                vec[i + 1] = v0 * fci + v1 * fcr
+            end
+
         end
+        
+        for h in 1:config.n_heads # multi-head attention
+
+            q = state.q[(h * head_size):((h + 1) * head_size)]
+            att = @view state.att[h, :]
+
+            for t in 0:pos
+
+                k = state.key_cache[loff, t * kv_dim, (div(h, kv_mul) * head_size):((div(h, kv_mul) + 1) * head_size)]
+
+                score = dot(q, k)/sqrt(head_size)
+                att[t] = score
+
+            end
+
+            softmax!(att, pos + 1)
+
+            xb = @view state.xb[(h * head_size):((h + 1) * head_size)]
+
+            for t in 0:pos
+
+                v = state.value_cache[loff, t * kv_dim, (div(h, kv_mul) * head_size):((div(h, kv_mul) + 1) * head_size)]
+                a = att[t]
+                
+                xb += a * v
+
+            end
+
+        end
+
+        state.xb2 = wo[l * dim * dim, :, :] * state.xb
+
+        x += state.xb2
+
+        rmsnorm!(state.xb, x, weights.rms_ffn_weight[l, :])
+
+        state.hb = weights.w1[l * dim * hidden_dim, :, :] * state.xb
+        state.hb2 = weights.w3[l * dim * hidden_dim, :, :] * state.xb
+
+        for i in 1:hidden_dim
+            val = state.hb[i]
+            val *= (1.0f / (1.0f + exp(-val)))
+            val *= state.hb2[i]
+            state.hb[i] = val
+        end
+
+        state.xb = weights.w2[l * dim * hidden_dim, :, :] * state.hb
+
+        x += state.xb
+        
     end
 
     rmsnorm!(state.x, state.x, weights.rms_final_weight) # final rmsnorm
-    #transform into logits
-    return nothing
+    
+    # classifier into logits
+    state.logits = weights.wcls * x
+    
+    return state.logits
+
 end
