4eee6cefbf0431b71f55d28a2c9ecbaa455f5f5a
Felipe Kempkes <felipe-berlin@hotmail.de>
2026-01-14 00:23:31 +0100

    Updated Talktollm to accept longer inputs.

--- src/Llama2.jl
+++ src/Llama2.jl
@@ -1,22 +1,55 @@
 module Llama2
 
-export Tokenizer, TokenIndex, compare_tokens, str_lookup, encode
-# Write your package code here.
+using StatsBase: wsample
+
+export Tokenizer, TokenIndex, compare_tokens, str_lookup, encode, talktollm
 
 include("structs.jl")
 include("tokenizer.jl")
 include("decode_transformer.jl")
 include("forward.jl")
 
+"""
+    talktollm(modelpath::String, vocabpath::String, prompt::String, max_tokens::Int)
+
+Generate text using a pretrained LLama2 transformer model. The function loads the
+model from `modelpath` and the corresponding tokenizer from `vocabpath`. It takes an initial 
+`prompt` string to start the text generation and generates up to `max_tokens` tokens.
+
+```jldoctest
+julia> using Llama2;
+
+julia> talktollm("/PATH/TO/MODEL.bin"
+                ,"/PATH/TO/VOCAB.bin"
+                ,"hey whats up?", 100);
+```
+
+"""
 function talktollm(modelpath::String, vocabpath::String, prompt::String, max_tokens::Int=50)
+    
+    transformer = Transformer(modelpath)
+    tok = Tokenizer(vocabpath, transformer.config.vocab_size-268)
 
-    t = Transformer(modelpath)
-    tok = Tokenizer(vocabpath, t.config.vocab_size-268)
-    input_tokens = encode(tok, prompt)
+    token = 1
 
-    return forward!(t, Int32(input_tokens[1]), Int32(0))
+    if !isempty(prompt)
+        input_tokens = encode(tok, prompt)
+    end
+
+    for pos in 1:max_tokens
+        forward!(transformer, Int32(token), Int32(pos))
+        if pos < length(input_tokens)
+            next = input_tokens[pos]
+        else
+            logits = transformer.state.logits ./ 0.8f0
+            softmax!(logits)
+            next = wsample(1:transformer.config.vocab_size, logits)
+        end
+        print(tok.vocab[next])
+        token = next
+    end
+
+    return nothing
 end
-
-export talktollm
 
 end
